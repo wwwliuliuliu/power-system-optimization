c99c103815b99432c9c1d4e51e9a5503a6e281d1
itaoxiaoran <35984267+itaoxiaoran@users.noreply.github.com>
2020-03-23 17:43:51 +0800

    Create no_use.py

---
+++ no_use.py
@@ -0,0 +1,6 @@
+###this is no use Python file
+#because this repository is a index
+# the functions of the this repository is described in the preview section
+Author = 'itaoxiaoran'
+email = 'ta0ran@163.com'
+# url = https://github.com/itaoxiaoran/power-system-optimization
